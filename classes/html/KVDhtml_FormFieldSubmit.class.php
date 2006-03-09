e720df767e878eafe9aa7721644031b412a49042
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2006-03-09 10:24:15 +0000

    Eerste versie

---
+++ classes/html/KVDhtml_FormFieldSubmit.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package KVD.html
+ * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * @version $Id$
+ */
+
+/**
+ * @package KVD.html
+ * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * @since 1.0.0
+ */
+class KVDhtml_FormFieldSubmit extends KVDhtml_FormField
+{
+    /**
+     * @return string
+     */
+    public function toHtml()
+    {
+        $this->toHtmlAttribValue();
+        $this->toHtmlAttribClass();
+        $this->toHtmlAttribDisabled();
+     
+        return sprintf($this->fieldFormat, 'submit', $this->name, $this->value, $this->class, '', $this->disabled);
+    }
+}
+?>
