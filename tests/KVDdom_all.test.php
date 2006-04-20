33a1fc20d039f8e6893949b32a49ec2d3b6be389
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2006-04-20 13:41:34 +0000

    Eerste versie

---
+++ tests/KVDdom_all.test.php
@@ -0,0 +1,37 @@
+<?php
+Error_Reporting (  E_ALL & ~E_STRICT);
+
+define ( 'KVD_CLASSES_DIR' , '/data/projects/kvd/mel/webapp/lib/kvd/classes/');
+define ( 'OEI_CLASSES_DIR' , '/data/projects/kvd/mel/webapp/lib/oei/classes/');
+
+require_once('simpletest/unit_tester.php');
+require_once('simpletest/reporter.php');
+require_once('simpletest/mock_objects.php');
+
+
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_IdentityMap.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_MapperFactory.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_MapperRegistry.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_DataMapper.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_ChangeableDataMapper.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_LogableDataMapper.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_Sessie.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_SystemFields.class.php');
+require_once( KVD_CLASSES_DIR . 'domain/KVDdom_DomainObjectCollection.class.php');
+
+
+require_once('domain/KVDdom_MapperFactory.test.php');
+require_once('domain/KVDdom_MapperRegistry.test.php');
+require_once('domain/KVDdom_IdentityMap.test.php');
+require_once( 'domain/KVDdom_DomainObjectCollection.test.php');
+//require_once('CAI_Sessie.test.php');
+
+$test = new GroupTest('KVDdom_AllTests');
+$test->addTestCase( new TestOfMapperFactory());
+$test->addTestCase( new TestOfMapperRegistry());
+$test->addTestCase( new TestOfGenericIdentityMap());
+$test->addTestCase( new TestOfDomainObjectCollection( ));
+//$test->addTestCase( new TestOfSessie());
+$test->run(new TextReporter());
+
+?>
