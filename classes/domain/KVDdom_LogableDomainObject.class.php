63edb706e44a9c393f107ec0ee92117e7baf62e7
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2006-06-06 09:28:54 +0000

    DO implementeert nu Nullable interface en kan goedgekeurd worden.

--- classes/domain/KVDdom_LogableDomainObject.class.php
+++ classes/domain/KVDdom_LogableDomainObject.class.php
@@ -14,7 +14,7 @@
  * @since 1.0.0
  */
 
-abstract class KVDdom_LogableDomainObject extends KVDdom_ChangeableDomainObject
+abstract class KVDdom_LogableDomainObject extends KVDdom_ChangeableDomainObject implements KVDdom_Nullable
 {
 
     /**
@@ -31,11 +31,26 @@ abstract class KVDdom_LogableDomainObject extends KVDdom_ChangeableDomainObject
     public function __construct ( $id , $sessie , $systemFields = null)
     {
         parent::__construct ($id , $sessie );
+        $this->id = $id;
+        $this->_sessie = $sessie;
         if ($systemFields === null) {
             $this->_systemFields = new KVDdom_SystemFields($this->_sessie->getGebruiker()->getGebruikersNaam());
         } else {
             $this->_systemFields = $systemFields;
         }
+        if ( $this->_systemFields->isCurrentRecord( ) ) {
+            $this->markClean( );
+        }
+    }
+
+    /**
+     * Markeert dit object als Approved
+     *
+     * Dit record zal door KVDdom_Sessie worden goedgeurd in de databank bij het verwerken van de UnitOfWork. Dit komt neer op het uitvoeren van een
+     */
+    protected function markApproved(  )
+    {
+        $this->_sessie->registerApproved( $this );
     }
     
     /**
@@ -44,6 +59,23 @@ abstract class KVDdom_LogableDomainObject extends KVDdom_ChangeableDomainObject
     public function getSystemFields()
     {
         return $this->_systemFields;
+    }
+
+    /**
+     * Keur het domeinobject goed.
+     */
+    public function approve( )
+    {
+        $this->markApproved( );
+        $this->_systemFields->setApproved( );
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isNull( )
+    {
+        return false;
     }
 
 }
