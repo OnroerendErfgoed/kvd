242d96ccb70d701df56647a270364e6d6f8be1f0
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2006-06-07 11:15:30 +0000

    Hernoemen van bestand. Toevoegen van rows en cols eigenschap.

---
+++ classes/html/KVDhtml_FormFieldTextarea.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package KVD.html
+ * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * @version $Id: KVDhtml_FormFieldTextArea.class.php,v 1.1 2006/01/12 12:30:14 Koen Exp $
+ */
+
+ /**
+ * @package KVD.html
+ * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * @since 1.0.0
+ */
+class KVDhtml_FormFieldTextarea extends KVDhtml_FormField
+{
+    /**
+     * @var string
+     */
+    protected $fieldFormat = "<textarea name=\"%s\" cols=\"%s\" rows=\"%s\"%s%s%s>\n%s\n</textarea>\n";
+
+    /**
+     * @var integer
+     */
+    private $rows;
+
+    /**
+     * @var integer
+     */
+    private $cols;
+    
+    /**
+     * @param array $fieldOptions
+     */
+    protected function setField(&$fieldOptions)
+    {
+        parent::setField($fieldOptions);
+        $this->rows = (isset($fieldOptions['rows'])) ? $fieldOptions['rows'] : 10;
+        $this->cols = (isset($fieldOptions['cols'])) ? $fieldOptions['cols'] : 40;
+    }
+    
+    /**
+     * @return string
+     */
+    public function toHtml()
+    {
+        $this->toHtmlAttribClass();
+        $this->toHtmlAttribReadonly();
+        $this->toHtmlAttribDisabled();
+     
+        return sprintf($this->fieldFormat, $this->name, $this->cols, $this->rows, $this->class, $this->readonly, $this->disabled, $this->value);
+    }
+}
+?>
