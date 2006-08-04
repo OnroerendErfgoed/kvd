3876178e6d7396f3d1b7890f2547e9ced1fc98a6
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2006-08-04 15:57:32 +0000

    Eerste versie

---
+++ classes/agavi/KVDag_SelectorOverzichtTableHelper.class.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package KVD.agavi
+ * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * @version $Id$
+ */
+
+/**
+ * @package KVD.agavi
+ * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * @since 3 jul 2006
+ */
+class KVDag_SelectorOverzichtTableHelper extends KVDag_PagedOverzichtTableHelper
+{
+    private $idField;
+    
+    private $formAction;
+    
+    public function __construct( $ctrl, $config , $pager)
+    {
+        parent::__construct( $ctrl, $config , $pager);
+        
+        if ( !isset( $config['formAction'] ) ) {
+            throw new InvalidArgumentException ( 'Er is geen formAction gespecifieerd!' );
+        }
+        $this->formAction = $config['formAction'];
+
+        $this->idField = isset( $config['idField'] ) ? $config['idField'] : 'id';
+
+        $this->idField .= '[]';
+        
+    }
+    public function genRowsForCollection( $collection , $generateActions = false )
+    {
+        $rows = array( );
+
+        foreach ( $collection as $domainObject ) {
+            $row = array( );
+            $fieldOptions = array ( 'name' => "{$this->idField}",
+                                    'value'=> $domainObject->getId( ) );
+            $selector = new KVDhtml_FormFieldCheckbox( $fieldOptions );
+            $row[] = $selector->toHtml( );
+            foreach ( $this->fieldsPerRow as $field ) {
+                $row[] = $this->getDataForFieldString( $domainObject, $field );
+            }
+            $rows[] = $row;
+        }
+        if ( count( $rows ) > 0 ) {
+            $this->_htmlTableHelper->setRows( $rows );
+            $this->_htmlTableHelper->setHeaders( $this->headers );
+        }
+    }
+
+    public function toHtml( $cssClasses = null ) 
+    {
+        $html = "<form method=\"POST\" action=\"{$this->formAction}\">\n";
+        $html .= parent::toHtml( $cssClasses );
+        $submit = new KVDhtml_FormFieldSubmit ( array ( 'name' =>   'relictSelecteren',
+                                                        'value' =>  'Selecteren'
+                                                       )
+                                                );
+        $html .= $submit->toHtml( );
+        $html .= "</form>";
+        return $html;
+    }
+    
+}
+?>
