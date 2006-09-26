32b5c81df6227d865d52e1d71f6ba5c62d371812
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2006-09-26 12:37:05 +0000

    Gewijzigde testrunners

--- tests/KVDdom_all.test.php
+++ tests/KVDdom_all.test.php
@@ -3,11 +3,14 @@ Error_Reporting (  E_ALL & ~E_STRICT);
 
 define ( 'KVD_CLASSES_DIR' , '/data/projects/kvd/kvd/classes/');
 define ( 'OEI_CLASSES_DIR' , '/data/projects/kvd/oei/trunk/classes/');
+define ( 'AGAVI_DIR' , '/opt/csw/php5/lib/php/agavi/');
 
 require_once('simpletest/unit_tester.php');
 require_once('simpletest/reporter.php');
 require_once('simpletest/mock_objects.php');
 
+require_once( AGAVI_DIR . 'core/AgaviObject.class.php' );
+require_once( AGAVI_DIR . 'database/DatabaseManager.class.php' );
 
 require_once( KVD_CLASSES_DIR . 'domain/KVDdom_IdentityMap.class.php');
 require_once( KVD_CLASSES_DIR . 'domain/KVDdom_MapperFactory.class.php');
@@ -27,16 +30,19 @@ require_once('domain/KVDdom_MapperFactory.test.php');
 require_once('domain/KVDdom_MapperRegistry.test.php');
 require_once('domain/KVDdom_IdentityMap.test.php');
 require_once( 'domain/KVDdom_DomainObjectCollection.test.php');
+require_once( 'domain/KVDdom_SqlLogger.test.php');
 require_once( 'domain/KVDdom_PDOChunkyQuery.test.php');
-//require_once('CAI_Sessie.test.php');
+require_once( 'domain/KVDdom_Sessie.test.php');
 
 $test = new GroupTest('KVDdom_AllTests');
 $test->addTestCase( new TestOfMapperFactory());
-//$test->addTestCase( new TestOfMapperRegistry());
+$test->addTestCase( new TestOfMapperRegistry());
 $test->addTestCase( new TestOfGenericIdentityMap());
-//$test->addTestCase( new TestOfDomainObjectCollection( ));
-//$test->addTestCase( new TestOfSessie());
+$test->addTestCase( new TestOfDomainObjectCollection( ));
+$test->addTestCase( new TestOfSessie());
+$test->addTestCase ( new TestOfSqlLogger( ) );
 $test->addTestCase( new TestOfPDOChunkyQuery());
+
 $test->run(new TextReporter());
 
 ?>
--- tests/KVDgis_all.test.php
+++ tests/KVDgis_all.test.php
@@ -5,9 +5,9 @@ require_once('simpletest/unit_tester.php');
 require_once('simpletest/reporter.php');
 require_once('simpletest/mock_objects.php');
 
-define ('GISMAP' , '/data/projects/kvd/mel/webapp/lib/kvd/classes/gis/');
-define ('GISTESTMAP' , '/data/projects/kvd/mel/webapp/lib/kvd/tests/gis/');
-define ('UTILMAP' , '/data/projects/kvd/mel/webapp/lib/kvd/classes/util/');
+define ('GISMAP' , '/data/projects/kvd/kvd/classes/gis/');
+define ('GISTESTMAP' , '/data/projects/kvd/kvd/tests/gis/');
+define ('UTILMAP' , '/data/projects/kvd/kvd/classes/util/');
 //define ('KVDGISMAPFILE' , 'c:/opt/kvd/tests/gis/KVDgis.map');
 //define ('KVDGISTEMPDIR' , 'c:/opt/tmp/');
 
@@ -17,7 +17,12 @@ define ('UTILMAP' , '/data/projects/kvd/mel/webapp/lib/kvd/classes/util/');
 //require_once(GISTESTMAP.'KVDgis_MsMapAjaxHandler.test.php');
 //require_once(GISTESTMAP.'exception/KVDgis_MsMapActionBestaatNietException.test.php');
 require_once ( UTILMAP . 'Gateway/KVDutil_Gateway.interface.php' );
+require_once ( GISMAP . '/geometry/KVDgis_GeomGeometry.class.php');
+require_once ( GISMAP . '/geometry/KVDgis_GeomPoint.class.php');
+require_once ( GISMAP . '/geometry/KVDgis_GeomMultiPoint.class.php');
+
 require_once ( GISTESTMAP.'geometry/KVDgis_GeomPoint.test.php');
+require_once ( GISTESTMAP.'geometry/KVDgis_GeomMultiPoint.test.php');
 require_once ( GISTESTMAP.'crab/KVDgis_Crab1Gateway.test.php');
 require_once ( GISTESTMAP.'crab/KVDgis_CrabCache.test.php');
 require_once ( GISTESTMAP.'crab/KVDgis_NullCrabCache.test.php');
@@ -27,6 +32,7 @@ $test = new GroupTest('KVDgis_AllTests');
 //$test->addTestCase( new TestOfMsMapState());
 //$test->addTestCase( new TestOfMsMapAjaxHandler());
 $test->addTestCase( new TestOfGeomPoint());
+$test->addTestCase( new TestOfGeomMultiPoint());
 $test->addTestCase( new TestOfCrab1Gateway( ) );
 $test->addTestCase( new TestofCrabCache( ) );
 $test->addTestCase( new TestofNullCrabCache( ) );
