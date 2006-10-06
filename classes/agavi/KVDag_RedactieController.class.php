bf8ba65121b3482753c9b83704e6b533179fcc98
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2006-10-06 08:31:52 +0000

    Hoofdzakelijk doc fixes.

--- classes/agavi/KVDag_RedactieController.class.php
+++ classes/agavi/KVDag_RedactieController.class.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * @package KVD.ag
- * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * @package KVD.agavi
+ * @copyright 2004-2006 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @version $Id$
  */
 
-
 /**
- * @package KVD.agavi
- * @author Koen Van Daele <koen.vandaele@lin.vlaanderen.be>
+ * KVDag_RedactieController 
+ * 
  * @since 19 jun 2006
+ * @package KVD.agavi
+ * @copyright 2004-2006 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
  */
 class KVDag_RedactieController
 {
@@ -29,21 +34,26 @@ class KVDag_RedactieController
     private $mapper;
 
     /**
-     * @var string $module
-     * @var string $subAction
-     * @var KVDdom_LogableDataMapper
+     * @param string $subAction
+     * @param KVDdom_LogableDataMapper
+     * @throws <b>InvalidArgumentException</b> Indien de mapper geen object is.
      */
     public function __construct ( $subAction , $mapper)
     {
         $this->subAction = $subAction;
-
+        
+        if ( !is_object( $mapper ) ) {
+            throw new InvalidArgumentException ( 'De mapper moet een object zijn.' );
+        }
+    
         $this->mapper = $mapper;
 
     }
 
     /**
-     * @var Request
-     * @var KVDdom_LogableDomainObject
+     * @var Request Het Request object in Agavi.
+     * @var KVDdom_LogableDomainObject Het object dat geredigeerd wordt.
+     * @throws <b>InvalidArgumentException</b> Indien er om een niet bestaande redactie gevraagd wordt.
      */
     public function execute( $req, $domainObject)
     {
