e0c931d480780249c74cd482213905a9a05a064d
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2007-09-19 11:10:13 +0000

    Toevoegen van een Sliding Pager, overgenomen uit OAR.

---
+++ classes/html/KVDhtml_SlidingPager.class.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * @package KVD.html
+ * @version $Id$
+ * @copyright 2004-2007 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDhtml_SlidingPager 
+ * 
+ * Een Sliding Pager die gebruikt moet worden in combinatie met een KVDdom_DomainObejctCollectionPager en het Agavi Framework.
+ * @package KVD.html
+ * @since 21 aug 2007
+ * @copyright 2004-2007 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class KVDhtml_SlidingPager {
+
+    /**
+     * pager 
+     * 
+     * @var KVDdom_DomainObjectCollectionPager
+     */
+    protected $pager;
+
+    /**
+     * ro 
+     * 
+     * @var AgaviRouting
+     */
+    protected $ro;
+
+    /**
+     * route 
+     * 
+     * @var String
+     */
+    protected $route;
+
+    /**
+     * lh 
+     * 
+     * @var KVDhtml_LinkHelper
+     */
+    protected $lh;
+
+    /**
+     * @param KVDdom_DomainObjectCollectionPager $pager 
+     * @param AgaviRouting $ro 
+     * @param String $route   Naam van de route die de overzichten genereert.
+     * @return void
+     */
+    public function __construct( KVDdom_DomainObjectCollectionPager $pager, AgaviRouting $ro, $route)
+    {
+        $this->pager = $pager;
+        $this->ro = $ro;
+        $this->route = $route;
+        $this->lh = new KVDhtml_LinkHelper( );
+    }
+
+    /**
+     * toHtml 
+     * 
+     * @param int $range 
+     * @return string
+     */
+    public function toHtml( $range = 5)
+    {
+        $html = '';
+        if ( $this->pager->getPage( ) > $this->pager->getFirstPage( ) ) {
+            $html .= $this->lh->genHtmlLink( $this->ro->gen( $this->route , array ( 'pagina' => $this->pager->getPrev( ) ) ) , 'Vorige');
+            $html .= ' [ ';
+            $html .= $this->lh->genHtmlLink( $this->ro->gen( $this->route , array ( 'pagina' => $this->pager->getFirstPage( ) ) ) , $this->pager->getFirstPage( ) ) . ' ';
+        } else {
+            $html .= 'Vorige [ ';
+        }
+	    
+        $html .= ($this->pager->getPage() > $this->pager->getFirstPage( ) + $range ) ? '.. ' : '';
+        
+		foreach ($this->pager->getPrevLinks($range) as $page) {
+			if ($page != $this->pager->getFirstPage( )) {
+                $html .= $this->lh->genHtmlLink( $this->ro->gen( $this->route , array ('pagina' => $page ) ),$page) . ' ';
+			}
+		}
+
+        $html .= '<strong>'.$this->pager->getPage( ).'</strong> ';
+			
+        foreach ($this->pager->getNextLinks($range ) as $page) {
+			if ($page != $this->pager->getLastPage()) {
+                $html .= $this->lh->genHtmlLink( $this->ro->gen( $this->route , array ('pagina' => $page ) ),$page) . ' ';
+			}
+		}	
+
+        $html .= ($this->pager->getPage() < $this->pager->getLastPage( ) - $range ) ? '.. ' : '';
+			 
+   		if ( $this->pager->getPage( ) < $this->pager->getLastPage( ) ) {
+            $html .= $this->lh->genHtmlLink( $this->ro->gen( $this->route , array ( 'pagina' => $this->pager->getLastPage( ) ) ) , $this->pager->getLastPage( ) );
+            $html .= ' ] ';
+            $html .= $this->lh->genHtmlLink( $this->ro->gen( $this->route , array ( 'pagina' => $this->pager->getNext( ) ) ) , 'Volgende' );
+  		} else {
+            $html .= ' ] Volgende';
+        }
+        return $html;
+    }
+}
+?>
