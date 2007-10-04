d04daa5d6091b0b7cc0a385e305d282da1a6bede
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2007-10-04 21:00:39 +0000

    FormHelper uitbreiden met een mogelijkheid om lege waarden in een select te plaatsen.

--- classes/html/KVDhtml_FormFieldSelect.class.php
+++ classes/html/KVDhtml_FormFieldSelect.class.php
@@ -23,6 +23,27 @@ class KVDhtml_FormFieldSelect extends KVDhtml_FormField
     private $multiple;
 
     /**
+     * empty 
+     * 
+     * @var boolean
+     */
+    private $empty;
+
+    /**
+     * emptyValue 
+     * 
+     * @var string
+     */
+    private $emptyValue;
+
+    /**
+     * emptyKey 
+     * 
+     * @var mixed
+     */
+    private $emptyKey;
+
+    /**
      * @var string
      */
     protected $fieldFormat = "<select name=\"%s\" id=\"%s\"%s%s%s%s>\n";
@@ -40,6 +61,9 @@ class KVDhtml_FormFieldSelect extends KVDhtml_FormField
         parent::setField($fieldOptions);
         $this->options = (isset($fieldOptions['options'])) ? $fieldOptions['options'] : array( );
         $this->multiple = (isset($fieldOptions['multiple'])) ? $fieldOptions['multiple'] : '';
+        $this->empty = (isset($fieldOptions['empty']) && $fieldOptions['empty'] == true ) ? true : false;
+        $this->emptyKey = (isset($fieldOptions['emptyKey'])) ? $fieldOptions['emptyKey'] : '';
+        $this->emptyValue = (isset($fieldOptions['emptyValue'])) ? $fieldOptions['emptyValue'] : '';
     }
 
     private function toHtmlAttribMultiple()
@@ -72,12 +96,26 @@ class KVDhtml_FormFieldSelect extends KVDhtml_FormField
         }
         
         $select = sprintf($this->fieldFormat,$this->name,$this->id, $this->class, $this->multiple, $this->disabled,$this->size);
+        $select .= $this->getEmptyOption( );
         foreach ($this->options as $value => $text) {
             $selected = ($value == $this->value) ? ' selected="selected"' : '';
             $select .= sprintf($this->optionFormat, $value, $selected, $text);
         }
         $select .= "</select>\n";
         return $select;
+    }
+
+    /**
+     * getEmptyOption 
+     * 
+     * @return string
+     */
+    private function getEmptyOption( )
+    {
+        if ( $this->empty == true ) {
+            return sprintf( $this->optionFormat, $this->emptyKey, '', $this->emptyValue );
+        }
+        return '';
     }
 }
 ?>
