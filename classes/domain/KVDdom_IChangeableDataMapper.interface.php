8b44e1711eee36af2f3c8875f5917cf1f21e4c80
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2007-10-26 09:23:32 +0000

    Toevoegen van interfaces voor datamappers.

---
+++ classes/domain/KVDdom_IChangeableDataMapper.interface.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package KVD.dom
+ * @version $Id$
+ * @copyright 2004-2007 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDdom_IChangeableDataMapper 
+ * 
+ * Deze class stelt een domainobject in staat zichzelf te laten opslaan in een datasource.
+ * Onder een datasource verstaan we een externe bron die gegevens in leven houdt buiten
+ * de php request cycle. Dit kan dus een databank, een bestand, een LDAP server, 
+ * een SOAP gateway of nog iets anders zijn.
+ * @package KVD.dom
+ * @since 19 okt 2007
+ * @copyright 2004-2007 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+interface KVDdom_IChangeableDataMapper extends KVDdom_IDataMapper
+{
+    /**
+     * insert 
+     *
+     * Voeg een nieuw object toe aan de datasource.
+     * @param KVDdom_ChangeableDomainObject $object 
+     * @return KVDdom_ChangeableDomainObject
+     */
+    public function insert( $object );
+
+    /**
+     * update 
+     *
+     * Bewerk een object in de datasource.
+     * @param KVDdom_ChangeableDomainObject $object 
+     * @return KVDdom_ChangeableDomainObject
+     */
+    public function update( $object );
+
+    /**
+     * delete 
+     * 
+     * Verwijder een object uit de datasource.
+     * @param KVDdom_ChangeableDomainObject $object 
+     * @return KVDdom_ChangeableDomainObject
+     */
+    public function delete( $object );
+
+}
+?>
---
+++ classes/domain/KVDdom_IDataMapper.interface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package KVD.dom
+ * @version $Id$
+ * @copyright 2004-2007 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDdom_IDataMapper 
+ * 
+ * Een KVDdom_IDataMapper is een class die een domainobject kan laden uit een datasource.
+ * Onder datasource verstaan we een externe bron die een object kan opslaan buiten
+ * de php request cycle. Dit kan dus een databank, een bestand, een LDAP server,
+ * een SOAP service, XML-RPC service of iets anders zijn.
+ * Vaak zal een class die deze methode implementeert nog andere methodes zoals findAll
+ * of findBy* hebben, maar deze zijn optioneel. Per class moet er gekeken worden welke
+ * methodes zinvol zijn.
+ * @package KVD.dom
+ * @since 19 okt 2007
+ * @copyright 2004-2007 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+interface KVDdom_IDataMapper
+{
+    /**
+     * findById 
+     * 
+     * @param   mixed   $id     Een unieke identifier die toelaat een object terug te vinden.
+     *                          Meestal is dit een integer, soms ook eens string.
+     * @return  KVDdom_DomainObject     Het gevonden object
+     * @throws  KVDdom_DomainObjectNotFoundException    Indien het gevraagde object niet kon gevonden worden.
+     */
+    public function findById( $id );
+
+}
+?>
