175f6db2d163d2e6638d2eac8b24aaaf5369ea7a
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2008-01-06 13:27:43 +0000

    Toevoegen script om wachtwoorden aan te maken.

---
+++ scripts/wachtwoord.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package KVD.util
+ * @category scripts
+ * @version $Id$
+ * @since 6 jan 2008
+ * @copyright 2004-2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * 
+ */
+require_once( '../classes/util/KVDutil_WachtwoordGenerator.class.php');
+
+$wg = new KVDutil_WachtwoordGenerator( );
+echo $wg->generate( );
+?>
