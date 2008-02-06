04e9bd83a4d52e469e610d142c72bc7ec9e1b69e
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2008-02-06 10:37:11 +0000

    AutloadGenerationTask afgewerkt. Zou volledig moeten werken.

--- phing/tasks/AutoloadGenerationTask.php
+++ phing/tasks/AutoloadGenerationTask.php
@@ -1,12 +1,59 @@
 <?php
+/**
+ * @package phing
+ * @version $Id$
+ * @copyright 2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
 require_once( "phing/Task.php");
+
+/**
+ * AutoloadGenerationTask 
+ * 
+ * @package phing
+ * @since 6 feb 2008
+ * @copyright 2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
 class AutoloadGenerationTask extends Task
 {
+    /**
+     * classes 
+     * 
+     * @var array
+     */
     private $classes = array( );
 
+    /**
+     * filesets 
+     * 
+     * @var array
+     */
     private $filesets = array( );
 
+    /**
+     * destinationFile 
+     * 
+     * @var string
+     */
     private $destinationFile;
+
+    /**
+     * outputType 
+     * 
+     * @var string
+     */
+    private $outputType;
+
+    /**
+     * outputName 
+     * 
+     * @var string
+     */
+    private $outputName;
 
     /**
      * Nested creator, creates a FileSet for this task
@@ -19,16 +66,56 @@ class AutoloadGenerationTask extends Task
         return $this->filesets[$num-1];
     }
 
+    /**
+     * setDestFile 
+     * 
+     * @param PhingFile $file 
+     * @return void
+     */
     public function setDestFile( PhingFile $file)
     {
         $this->destinationFile = $file;
     }
 
+    /**
+     * setOutputType 
+     * 
+     * @param string $type 
+     * @return void
+     */
+    public function setOutputType( $type )
+    {
+        $this->outputType = $type;
+    }
+
+    /**
+     * setOutputName 
+     * 
+     * @param string $name 
+     * @return void
+     */
+    public function setOutputName( $name )
+    {
+        $this->outputName = $name;
+    }
+
+    /**
+     * init 
+     * 
+     * @return void
+     */
     public function init( )
     {
         // do nothing
     }
 
+    /**
+     * Validate input. Check if filesets, destinationfile and outputname are present.
+     * Set outputtype to default value 'Array' if not present.
+     * 
+     * @throws BuildException - If certain parameters are not set.
+     * @return void
+     */
     private function validate( )
     {
         if ( empty($this->filesets) ) {
@@ -37,8 +124,19 @@ class AutoloadGenerationTask extends Task
         if ( !isset( $this->destinationFile ) ) {
             throw new BuildException("You must specify a destination file.", $this->getLocation( ));
         }
+        if ( !isset( $this->outputType ) ) {
+            $this->outputType = 'Array';
+        }
+        if ( !isset( $this->outputName ) ) {
+            throw new BuildException("You must specify a name for the output variable or function.", $this->getLocation( ));
+        }
     }
 
+    /**
+     * Generate the autoloader. 
+     * 
+     * @return void
+     */
     public function main( )
     {
         $this->validate( );
@@ -52,13 +150,19 @@ class AutoloadGenerationTask extends Task
                 $this->classes = array_merge( $this->classes, $this->getClassesFromFile( $f->getAbsolutePath( )));
 	        }
         }
-        $creator = new AutoloadArrayCreator( );
+        $creator = AutoloadCreator::factory( $this->outputType, $this->outputName );
         $creator->create( $this->classes );
         $result = $creator->getResult( );
         $this->writeData( $result );
         $this->log( 'The autoloader contains ' . count( $this->classes ) . ' classes and interfaces.' );
     }
 
+    /**
+     * Get all classes and interfaces from a file
+     * 
+     * @param string $path 
+     * @return array An array of class names and interface names found.
+     */
     private function getClassesFromFile( $path )
     {
         $classes = array( );
@@ -77,29 +181,159 @@ class AutoloadGenerationTask extends Task
         return $classes;
     }
 
+    /**
+     * Write data to the destination file.
+     * 
+     * @param string $data 
+     * @throws BuildException - If the destination file could not be written.
+     * @return void
+     */
     private function writeData( $data )
     {
-        file_put_contents($this->destinationFile->getAbsolutePath( ), $data );
+        if ( !@file_put_contents($this->destinationFile->getAbsolutePath( ), $data ) ) {
+            throw new BuildException( 'Unable to write to the specified destination file.' );
+        }
     }
 }
 
+/**
+ * AutoloadCreator 
+ * 
+ * @package phing
+ * @subpackage 
+ * @since 6 feb 2008
+ * @copyright 2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
 abstract class AutoloadCreator
 {
+    /**
+     * result 
+     * 
+     * @var string
+     */
     protected $result;
 
-    abstract public function create( $data );
+    /**
+     * Name to use for the output variable or function.
+     * 
+     * @var string
+     */
+    protected $outputName;
 
+    /**
+     * create 
+     * 
+     * @param array $classes 
+     * @return void
+     */
+    abstract public function create( array $classes );
+
+    /**
+     * __construct 
+     * 
+     * @param string $outputName 
+     * @return void
+     */
+    public function __construct( $outputName )
+    {
+        $this->setOutputName( $outputName );
+    }
+
+    /**
+     * getResult 
+     * 
+     * @return string
+     */
     public function getResult( )
     {
         return $this->result;
     }
+
+    /**
+     * setOutputName 
+     * 
+     * @param string $name 
+     * @return void
+     */
+    public function setOutputName( $name )
+    {
+        $this->outputName = $name;
+    }
+
+    /**
+     * factory 
+     * 
+     * @param string $type          Type of creator to return. Can be 'Array' or 'Function'. Defaults to 'Array'.
+     * @param string $outputName    Name for the output variable or function
+     * @return AutoloadCreator
+     */
+    public static function factory( $type , $outputName)
+    {
+        switch ( $type ) {
+            case 'Function':
+                return new AutoloadFunctionCreator( $outputName );
+                break;
+            case 'Array':
+            default:
+                return new AutoloadArrayCreator( $outputName );
+                break;
+        }
+    }
+
 }
 
+/**
+ * AutoloadArrayCreator 
+ * 
+ * @package phing
+ * @since 6 feb 2008
+ * @copyright 2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
 class AutoloadArrayCreator extends AutoloadCreator
 {
-    public function create( $data )
+    /**
+     * create 
+     * 
+     * @param array $classes 
+     * @return void
+     */
+    public function create( array $classes )
     {
-        $this->result = "<?php \n" . var_export( $data, true ) . "?>";
+        $this->result = "<?php \n\${$this->outputName}=" . var_export( $classes, true ) . "?>";
+    }
+}
+
+/**
+ * AutoloadFunctionCreator 
+ * 
+ * @package phing
+ * @since 6 feb 2008
+ * @copyright 2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class AutoloadFunctionCreator extends AutoloadCreator
+{
+    /**
+     * create 
+     * 
+     * @param array $classes 
+     * @return void
+     */
+    public function create( array $classes )
+    {
+        $this->result = 
+        "<?php \n " .
+        "function {$this->outputName}( \$class )  {\n" . 
+        "\tstatic \$autoloads = " .var_export( $classes, true) . ";\n" .
+        "\tif(isset(\$autoloads[\$class])) {\n" .
+        "\t\trequire(\$autoloads[\$class]);\n".
+        "\t}\n".
+        "}\n";
     }
 }
 ?>
