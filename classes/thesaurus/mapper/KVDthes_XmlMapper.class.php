ef15161a9f14a4fa9da63e7a33a955d7b966242f
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2008-09-25 14:21:39 +0000

    Kleine correcte inladen termQualifier.

--- classes/thesaurus/mapper/KVDthes_XmlMapper.class.php
+++ classes/thesaurus/mapper/KVDthes_XmlMapper.class.php
@@ -138,11 +138,10 @@ abstract class KVDthes_XmlMapper implements KVDthes_IDataMapper
             $id = $term->getElementsByTagName( 'termId' )->item( 0 )->nodeValue;
             $name = $term->getElementsByTagName( 'termName' )->item( 0 )->nodeValue;
             $language = $term->getElementsByTagName( 'termLanguage' )->item( 0 )->nodeValue;
-            $qualifier = $term->getElementsByTagName( 'termQualifier')->length > 0 ? $term->getElementsByTagName( 'termQualifier' )->item( 0 )->nodeValue : null;
-            if ( $qualifier === '' ) {
-                $qualifier = null;
-            }
-            $sortKey = $term->getElementsByTagName( 'termSortKey')->length > 0 ? $term->getElementsByTagName( 'termSortKey' )->item( 0 )->nodeValue : null;
+            $list = $this->xp->query( 'termQualifier', $term );
+            $qualifier = ( $list->length > 0 ) ? $list->item(0)->nodeValue : null;
+            $list = $this->xp->query( 'termSortKey', $term );
+            $sortKey = ( $list->length > 0 ) ? $list->item(0)->nodeValue : null;
             $termType = $this->getReturnType( );
             $thesaurus = $this->doLoadThesaurus( );
             $termObj = new $termType( $this->sessie , $id , $name , $qualifier, $language, $sortKey, null, null, $thesaurus);
