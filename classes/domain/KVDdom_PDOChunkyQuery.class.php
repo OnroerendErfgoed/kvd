3d965c37d73a1675d406e34e8b1d70f0a85a970e
standadi <standadi@c40535f3-310c-0410-a3fa-a148391524b6>
2008-10-03 14:51:31 +0000

    Correctie van regxp, robuster #67

--- classes/domain/KVDdom_PDOChunkyQuery.class.php
+++ classes/domain/KVDdom_PDOChunkyQuery.class.php
@@ -160,8 +160,8 @@ class KVDdom_PDOChunkyQuery
         if ( stripos( $sql, 'DISTINCT') !== FALSE ) {
             $idField = 'DISTINCT ' . $idField;
         }
-        $sql = preg_replace( '/(SELECT).*?(FROM)/is' , 'SELECT COUNT('.$idField.') FROM' , $sql , 1);
-        $sql = preg_replace ( '/ ORDER.*/is','',$sql);
+        $sql = preg_replace( '/(SELECT).*(FROM)/is' , 'SELECT COUNT('.$idField.') FROM' , $sql , 1);
+        $sql = preg_replace ( '/[\n\s]*ORDER.*/is','',$sql);
         $this->logger->log ( $sql );
         return $sql;
     }
