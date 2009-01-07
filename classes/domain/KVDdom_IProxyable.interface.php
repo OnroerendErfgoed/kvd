bdeb16ed63dfe93b883fe8ce1dc01685e4e6fb70
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2009-01-07 07:48:15 +0000

    Interface voor proxy objecten. Nodig voor de ErfgoedGrafe.

---
+++ classes/domain/KVDdom_IProxyable.interface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     KVD.domain
+ * @version     $Id$
+ * @copyright   2004-2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDdom_IProxyable 
+ * 
+ * Deze interface kan gebruikt worden om aan te geven of een object al dan niet volledig geladen is.
+ * @package     KVD.domain
+ * @since       okt 2008
+ * @copyright   2004-2008 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+interface KVDdom_IProxyable extends KVDdom_IDomainObject
+{
+    /**
+     * isProxy 
+     * 
+     * Is dit een echt domainobject of is dit gewoon een proxy object dat enkel beschikt over het type en het id van het domainobject?
+     * @return  boolean     False indien het een echt domainobject is, true indien het een proxy object is.
+     */
+    public function isProxy( );
+
+    /**
+     * newProxy 
+     * 
+     * Maak een nieuw proxyobject voor dit id.
+     * @param   integer             $id     Id nummer dat het proxy object moet hebben.
+     * @return  KVDdom_IProxyable   Een object dat de proxyable interface implementeert.
+     */
+    public function newProxy( $id );
+}
+?>
