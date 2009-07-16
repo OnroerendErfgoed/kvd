07f1d19ff3f61a8d94cfe9f30397b38b3fdf82f9
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2009-07-16 11:50:57 +0000

    Toevoegen van een class die srid als constanten heeft.

---
+++ classes/gis/util/KVDgis_UtilSrid.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package 	KVD.gis
+ * @subpackage 	util
+ * @version 	$Id$
+ * @copyright 	2009 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author 	Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
+ * @license 	http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDgis_UtilSrid
+ *
+ * @package     KVD.gis
+ * @since       16 jul 2009
+ * @copyright   2009 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class KVDgis_UtilSrid {
+
+    const LAMBERT72 = '31370';
+
+    const WGS84 = '4326';
+    
+}
+?>
