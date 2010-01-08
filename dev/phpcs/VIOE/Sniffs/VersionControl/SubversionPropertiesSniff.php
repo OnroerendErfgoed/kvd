be69fd1b987764dd685a704842079016364e45d9
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2010-01-08 14:56:49 +0000

    Toevoegen van een begin van een VIOE Coding standard. Heeft php_codesniffer nodig.

---
+++ dev/phpcs/VIOE/Sniffs/VersionControl/SubversionPropertiesSniff.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * VIOE_Sniffs_VersionControl_SubversionPropertiesSniff 
+ * 
+ * @package     KVD.dev
+ * @subpackage  phpcs
+ * @version     $Id$
+ * @copyright   2009 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * VIOE_Sniffs_VersionControl_SubversionPropertiesSniff 
+ * 
+ * @package     KVD.dev
+ * @subpackage  phpcs
+ * @since       8 jan 2009
+ * @copyright   2009 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class VIOE_Sniffs_VersionControl_SubversionPropertiesSniff extends Generic_Sniffs_VersionControl_SubversionPropertiesSniff
+{
+    protected $properties = array( 'svn:keywords'  => 'Id',
+                                   'svn:eol-style' => 'LF'
+                                 );
+}
+?>
---
+++ dev/phpcs/VIOE/VIOECodingStandard.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * VIOE Coding Standard.
+ * 
+ * @package     KVD.dev
+ * @subpackage  phpcs
+ * @version     $Id$
+ * @copyright   2009 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link        http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * VIOE Coding Standard
+ * 
+ * @package     KVD.dev
+ * @subpackage  phpcs
+ * @since       8 jan 2009
+ * @copyright   2009 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link        http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PHP_CodeSniffer_Standards_VIOE_VIOECodingStandard extends PHP_CodeSniffer_Standards_CodingStandard {
+    /**
+     * Return a list of external sniffs to include with this standard.
+     *
+     * The VIOE coding standard uses some generic sniffs, and
+     * some custom sniffs.
+     * 
+     * @return array
+     */
+    public function getIncludedSniffs( ) 
+    {
+        return array( 
+                    'Generic'
+                    );
+    }
+
+    /**
+     * Return a list of external sniffs to exclude with this standard.
+     *
+     * The VIOE coding standard uses some generic sniffs, and
+     * some custom sniffs.
+     * 
+     * @return array
+     */
+    public function getExcludedSniffs( )
+    {
+        return array(
+                    'Generic/Sniffs/Functions/OpeningFunctionBraceKernighanRitchieSniff.php',
+                    'Generic/Sniffs/VersionControl/SubversionPropertiesSniff.php'
+                    );
+    }
+}
+?> 
