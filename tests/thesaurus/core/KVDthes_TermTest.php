7bf112a199c031ac560fb81e60a049662ddc1487
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2010-01-20 12:48:36 +0000

    Gecorrigeerd UT voor thes term.

--- tests/thesaurus/core/KVDthes_TermTest.php
+++ tests/thesaurus/core/KVDthes_TermTest.php
@@ -16,7 +16,8 @@ class KVDthes_TermTest extends PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $sessie = $this->getMock( 'KVDthes_Sessie' );
-        $this->object = new KVDthes_TestTerm( 0, $sessie, 'TestTerm', null, 'Uniek');
+        $termType = new KVDthes_TermType( 'PT', 'voorkeursterm' );
+        $this->object = new KVDthes_TestTerm( 507, $sessie, 'kapellen', $termType, 'klein erfgoed');
     }
 
     /**
@@ -29,228 +30,43 @@ class KVDthes_TermTest extends PHPUnit_Framework_TestCase
     {
     }
 
-    /**
-     * @todo Implement testIsLoadState().
-     */
-    public function testIsLoadState() {
+    public function testLoadState() {
         $this->assertTrue( $this->object->isLoadState( KVDthes_Term::LS_TERM ) );
         $this->assertFalse( $this->object->isLoadState( KVDthes_Term::LS_REL ) );
         $this->object->setLoadState( KVDthes_Term::LS_REL );
         $this->assertTrue( $this->object->isLoadState( KVDthes_Term::LS_REL ) );
     }
 
-    /**
-     * @todo Implement testCheckRelations().
-     */
-    public function testCheckRelations() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testCheckScopeNote().
-     */
-    public function testCheckScopeNote() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testCheckSourceNote().
-     */
-    public function testCheckSourceNote() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testSetLoadState().
-     */
-    public function testSetLoadState() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
     public function testGetTerm() {
-        $this->assertEquals( 'TestTerm', $this->object->getTerm( ) );
+        $this->assertEquals( 'kapellen', $this->object->getTerm( ) );
     }
 
     public function testGetQualifier() {
-        $this->assertEquals( 'Uniek', $this->object->getQualifier( ) );
+        $this->assertEquals( 'klein erfgoed', $this->object->getQualifier( ) );
     }
 
     public function testGetQualifiedTerm( ) {
-        $this->assertEquals( 'TestTerm (Uniek)', $this->object->getQualifiedTerm( ) );
+        $this->assertEquals( 'kapellen (klein erfgoed)', $this->object->getQualifiedTerm( ) );
     }
 
     public function testGetId() {
-        $this->assertEquals( 0 , $this->object->getId( ) );
+        $this->assertEquals( 507 , $this->object->getId( ) );
     }
 
-    /**
-     * @todo Implement testGetScopeNote().
-     */
-    public function testGetScopeNote() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testGetSourceNote().
-     */
-    public function testGetSourceNote() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testAddRelation().
-     */
-    public function testAddRelation() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testAddScopeNote().
-     */
-    public function testAddScopeNote() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testAddSourceNote().
-     */
-    public function testAddSourceNote() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testAccept().
-     */
-    public function testAccept() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testAcceptSimple().
-     */
-    public function testAcceptSimple() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testIsPreferredTerm().
-     */
-    public function testIsPreferredTerm() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testGetPreferredTerm().
-     */
-    public function testGetPreferredTerm() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testGetNonPreferredTerms().
-     */
-    public function testGetNonPreferredTerms() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testGetRelatedTerms().
-     */
-    public function testGetRelatedTerms() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testGetLanguage().
-     */
     public function testGetLanguage() {
-        $this->assertEquals( 'Nederlands', $this->object->getLanguage( ) );
+        $this->assertEquals( 'nl-BE', $this->object->getLanguage( ) );
     }
 
-    /**
-     * @todo Implement testHasNTRelations().
-     */
-    public function testHasNTRelations() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testHasBTRelations().
-     */
-    public function testHasBTRelations() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testGetClass().
-     */
     public function testGetClass() {
         $this->assertEquals( 'KVDthes_TestTerm', $this->object->getClass( ) );
     }
 
-    /**
-     * @todo Implement testGetOmschrijving().
-     */
     public function testGetOmschrijving() {
-        $this->assertEquals( 'TestTerm (Uniek)', $this->object->getOmschrijving( ) );
+        $this->assertEquals( 'kapellen (klein erfgoed)', $this->object->getOmschrijving( ) );
     }
 
-    /**
-     * @todo Implement test__toString().
-     */
     public function test__toString() {
-        $this->assertEquals( 'TestTerm (Uniek)', $this->object->__toString( ) );
+        $this->assertEquals( 'kapellen (klein erfgoed)', $this->object->__toString( ) );
     }
 }
 ?>
