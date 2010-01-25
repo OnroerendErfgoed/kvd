20eebe8053fbf12ebe06812f85dac719ae99c63c
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2010-01-25 20:07:44 +0000

    Toevoegen van een test voor KVDhtml_Tools.

---
+++ tests/html/KVDhtml_ToolsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once ( 'PHPUnit/Framework.php' );
+
+class ToolsTest extends PHPUnit_Framework_TestCase
+{
+    private $testData;
+    
+    function setUp( )
+    {
+        $this->testData = array (  'Dit is een test.'       =>  'Dit is een test.' ,
+                                    'Lena > Mira'           =>  'Lena &gt; Mira' ,
+                                    'Luka & Felix'          =>  'Luka &amp; Felix' );
+    }
+
+    function tearDown( )
+    {
+        $this->testData = null;
+    }
+
+    public function testOut( )
+    {
+        foreach ( $this->testData as $input => $output ) {
+            $this->assertEquals ( $output, KVDhtml_Tools::out( $input) );
+        }
+    }
+
+    public function testOutImplode( )
+    {
+        $this->assertEquals('Dit is een test. | Lena &gt; Mira | Luka &amp; Felix',  KVDhtml_Tools::outImplode( $this->testData , ' | ' ) );
+        $this->assertEquals('Dit is een test. <br/> Lena &gt; Mira <br/> Luka &amp; Felix', KVDhtml_Tools::outImplode( $this->testData , ' <br/> ' ) );
+    }
+}
+?>
