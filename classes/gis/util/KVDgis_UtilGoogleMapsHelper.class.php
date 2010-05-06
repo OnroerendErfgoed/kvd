4795feeec45110d505bc360ac84fe0c98ccc8e1c
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2010-05-06 13:25:17 +0000

    Wijzigigen aan de class om google maps links aan te maken.

--- classes/gis/util/KVDgis_UtilGoogleMapsHelper.class.php
+++ classes/gis/util/KVDgis_UtilGoogleMapsHelper.class.php
@@ -22,24 +22,56 @@ class KVDgis_UtilGoogleMapsHelper
     /**
      * getStaticMap 
      * 
-     * @param   array   $parameters     De sleutels 'base_url' en 'size' moeten 
-     *                                  altijd aanwezig zijn.  
-     * @param   array   $geometries     Een array van geometries. Momenteel worden 
-     *                                  enkel {@link KVDgis_GeomPoint} objecten ondersteund.
+     * @param   array               $parameters     De sleutels 'base_url' en 'size' moeten 
+     *                                              altijd aanwezig zijn.  
+     * @param   KVDgis_GeomPoint    $punt           Het punt dat op de kaart 
+     *                                              moet geplaatst worden.
      * @return  string
      */
-    public static function getStaticMap( array $parameters, array $geometries )
+    public static function getStaticMap( array $parameters, KVDgis_GeomPoint $punt )
     {
         self::checkStaticParameters( $parameters );
-        $m = array( );
-        foreach ( $geometries as $geom ) {
-            $m[] = $geom->getY( ). ', ' . $geom->getX( );
+        if ( !$punt->getSrid( ) == KVDgis_UtilSrid::WGS84 ) {
+            throw new InvalidArgumentException( 'U kunt enkel punten in WGS84 formaat zetten op google maps.' );
         }
-        $markers = implode( '|', $m );
-        $parameters['markers'] = $markers;
+        $parameters['markers'] = $punt->getY( ). ',' . $punt->getX( );
         $base = $parameters['base_url'];
         unset( $parameters['base_url'] );
         return $base . '?' . http_build_query( $parameters, '', '&amp;' );
+    }
+
+    /**
+     * getLinkToGoogleMaps 
+     * 
+     * @param   array               $parameters     De sleutel 'base_url' moet 
+     *                                              altijd aanwzig zijn. Andere mogelijkheden zijn 'label' en andere 
+     *                                              parameters die door google maps ondersteund worden.
+     * @param   KVDgis_GeomPoint    $punt 
+     * @return  string
+     */
+    public static function getLinkToGoogleMaps( array $parameters, KVDgis_GeomPoint $punt )
+    {
+        self::checkLinkParameters( $parameters );
+        if ( !$punt->getSrid( ) == KVDgis_UtilSrid::WGS84 ) {
+            throw new InvalidArgumentException( 'U kunt enkel punten in WGS84 formaat zetten op google maps.' );
+        }
+        $q = $punt->getY( ). ',' . $punt->getX( );
+        
+        if ( isset( $parameters['label'] ) ) {
+            $q .= ' (' . str_replace(array( '(',')' ), '', $parameters['label']) . ')';
+            unset( $parameters['label'] );
+        }
+        $parameters['q'] = $q;
+        $base = $parameters['base_url'];
+        unset( $parameters['base_url'] );
+        return $base . '?' . http_build_query( $parameters, '', '&amp;' );
+    }
+
+    private static function checkLinkParameters( array $parameters )
+    {
+        if ( !isset( $parameters['base_url'] ) ) {
+            throw new InvalidArgumentException( 'base_url' );
+        }
     }
 
     /**
