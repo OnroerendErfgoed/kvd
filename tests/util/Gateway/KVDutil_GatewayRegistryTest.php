8979a12eafc209c22ef08ad71f7fa5f0ab948af9
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2011-02-08 12:18:19 +0000

    Oplossing en Unit Test voor #88.

--- tests/util/Gateway/KVDutil_GatewayRegistryTest.php
+++ tests/util/Gateway/KVDutil_GatewayRegistryTest.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * @package     KVD.util
+ * @subpackage  gateway
+ * @version     $Id$
+ * @copyright   2011 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDutil_GatewayRegistryTest 
+ * 
+ * @package     KVD.util
+ * @subpackage  gateway
+ * @since       1.4.2
+ * @copyright   2011 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
 class KVDutil_GatewayRegistryTest extends PHPUnit_Framework_TestCase
 {
     private $gateway;
@@ -7,7 +26,7 @@ class KVDutil_GatewayRegistryTest extends PHPUnit_Framework_TestCase
     
     private $registry;
 
-    function setUp( )
+    public function setUp( )
     {
         $sessie = new StdClass();
 
@@ -19,12 +38,11 @@ class KVDutil_GatewayRegistryTest extends PHPUnit_Framework_TestCase
 
         $this->gateway = new KVDutil_GatewayTestGateway ( $parameters );
 
-        $this->factory->expects( $this->once() )->method( 'createGateway' )->will( $this->returnValue( $this->gateway ) );
         $this->registry = new KVDutil_GatewayRegistry ( $this->factory );
         
     }
 
-    function tearDown( )
+    public function tearDown( )
     {
         $this->gateway = null;
         $this->factory = null;
@@ -32,10 +50,27 @@ class KVDutil_GatewayRegistryTest extends PHPUnit_Framework_TestCase
        
     }
 
-    function testGetGateway()
+    public function testGetGateway()
     {
+        $this->factory->expects( $this->once() )->method( 'createGateway' )->will( $this->returnValue( $this->gateway ) );
         $this->assertSame ($this->registry->getGateway( 'KVDutil_GatewayTestGateway') , $this->gateway );
         $this->assertSame ($this->registry->getGateway( 'KVDutil_GatewayTestGateway') , $this->gateway );
+    }
+
+    /**
+     * Controleer of het serialiseren van een gateway registry er voor zorgt 
+     * dat de geladen gateways niet mee opgeslagen worden en achteraf terug 
+     * worden aangemaakt.
+     * 
+     * @return void
+     */
+    public function testSerialise(  )
+    {
+        $this->factory->expects( $this->exactly( 2 ) )->method( 'createGateway' )->will( $this->returnValue( $this->gateway ) );
+        $gw = $this->registry->getGateway( 'KVDutil_GatewayTestGateway' );
+        $tmp = serialize($this->registry);
+        $reg = unserialize($tmp);
+        $gw2 = $this->registry->getGateway( 'KVDutil_GatewayTestGateway' );
     }
 
 
