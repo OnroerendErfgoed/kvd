79c661e8a69395031b113be228d6d0864bbfceee
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2011-02-28 21:12:54 +0000

    Exception voor sessies.

---
+++ classes/domain/sessie/KVDdom_Sessie_Exception.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     KVD.dom
+ * @subpackage  sessie
+ * @version     $Id$
+ * @copyright   2011 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * Exception die problemen met een sessie signaleert.
+ * 
+ * @package     KVD.dom
+ * @subpackage  sessie
+ * @since       1.4
+ * @copyright   2011 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class KVDdom_Sessie_Exception extends KVDdom_Exception
+{
+
+}
+?>
