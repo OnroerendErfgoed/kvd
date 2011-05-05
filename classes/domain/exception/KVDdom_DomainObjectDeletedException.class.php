6feef504582994c82b7cbc62ef1b76da5d743266
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2011-05-05 21:26:54 +0000

    Toevoegen exception die kan aangeven dat een object verwijderd werd.

---
+++ classes/domain/exception/KVDdom_DomainObjectDeletedException.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     KVD.dom
+ * @subpackage  exception
+ * @version     $Id$
+ * @copyright   2011 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDdom_DomainObjectDeletedException 
+ * 
+ * @package     KVD.dom
+ * @subpackage  exception
+ * @since       0.1.0
+ * @copyright   2011 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author      Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class KVDdom_DomainObjectDeletedException extends KVDdom_DomainObjectNotFoundException
+{
+
+}
+?>
