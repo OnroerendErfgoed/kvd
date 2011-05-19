8a0defc61f880e4d61a33d8fec2f0d258d25f7d1
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2011-05-19 14:28:05 +0000

    Foute parameter werd doorgegeven.

--- classes/util/Gateway/KVDutil_GatewayFactory.class.php
+++ classes/util/Gateway/KVDutil_GatewayFactory.class.php
@@ -54,7 +54,7 @@ class KVDutil_GatewayFactory {
             $m = $f['method'];
             if ( array_key_exists( 'class', $f ) ) {
                 $c = $f['class'];
-                return call_user_func_array( array( $c, $m ), $this->config['gateway'] ); 
+                return call_user_func_array( array( $c, $m ), array( $this->config[$gateway] ) ); 
             } else {
                 return $m($this->config[$gateway] );
             }
