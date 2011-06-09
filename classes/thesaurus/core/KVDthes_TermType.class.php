788593e04c68e3a1b5bec5bd65b9756ffb3f4ff4
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2011-06-09 11:19:40 +0000

    KVDthes_TermType in apart bestand geplaatst.

---
+++ classes/thesaurus/core/KVDthes_TermType.class.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package    KVD.thes
+ * @subpackage Core
+ * @version    $Id$
+ * @copyright  2009-2011 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author     Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDthes_TermType 
+ * 
+ * @package    KVD.thes
+ * @subpackage Core
+ * @since      16 apr 2009
+ * @copyright  2009-2001 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author     Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class KVDthes_TermType extends KVDdom_ValueDomainObject
+{
+    /**
+     * __construct 
+     * 
+     * @param   string  $id 
+     * @param   string  $type 
+     * @return  KVDthes_TermType
+     */
+    public function __construct( $id, $type = 'Onbepaald' )
+    {
+        $this->id = $id;
+        $this->type=$type;
+    }
+
+    /**
+     * getType 
+     * 
+     * @return  string
+     */
+    public function getType( )
+    {
+        return $this->type;
+    }
+
+    /**
+     * getOmschrijving 
+     * 
+     * @return  string
+     */
+    public function getOmschrijving( )
+    {
+        return $this->type;
+    }
+
+    /**
+     * newNull 
+     * 
+     * @return  KVDthes_TermType
+     */
+    public static function newNull( )
+    {
+        return new KVDthes_TermType( 'ND', 'Non Descriptor');
+    }
+    
+}
+?>
