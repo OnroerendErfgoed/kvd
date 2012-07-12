31ffce00c5669c8521fc3974a22963d911e4a7c6
vandaeko <vandaeko@c40535f3-310c-0410-a3fa-a148391524b6>
2012-07-12 13:11:46 +0000

    Toevoegen van do en dm voor matches met de Catalogue of Life. Refs #110.

---
+++ classes/do/KVDdo_ThesCOL.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    KVD.do
+ * @subpackage Thes
+ * @version    $Id$
+ * @copyright  2012 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author     Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDDo_ThesCOL
+ * 
+ * Deze class stelt een concept uit de Catalogue of Life voor.
+ *
+ * @package    KVD.do
+ * @subpackage Thes
+ * @since      1.6
+ * @copyright  2012 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author     Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class KVDdo_ThesCOL extends KVDthes_Concept
+{
+
+}
