74f05f805e3063d0dd781047fde37392494bee02
Koen Van Daele <koen_van_daele@telenet.be>
2013-11-18 16:11:31 +0100

    Toevoegen van de TMCT. Refs #122.

---
+++ classes/do/KVDdo_ThesTMCT.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    KVD.do
+ * @subpackage Thes
+ * @copyright  2013 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author     Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
+ * KVDDo_ThesTMCT
+ * 
+ * Deze class stelt een concept uit de Thesaurus of Maritime Craft Types van 
+ * English Heritage voor.
+ *
+ * @package    KVD.do
+ * @subpackage Thes
+ * @since      1.8
+ * @copyright  2013 {@link http://www.vioe.be Vlaams Instituut voor het Onroerend Erfgoed}
+ * @author     Koen Van Daele <koen.vandaele@rwo.vlaanderen.be> 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class KVDdo_ThesTMCT extends KVDthes_Concept
+{
+
+}
