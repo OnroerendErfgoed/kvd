c3bb94320dc77e585ce0c9e9eeb412103894e68e
Koen Van Daele <koen_van_daele@telenet.be>
2014-06-26 15:17:16 +0200

    Nog meer unit tests.

--- test/tests/util/KVDutil_HuisnummerFacade2Test.php
+++ test/tests/util/KVDutil_HuisnummerFacade2Test.php
@@ -127,12 +127,12 @@ class KVDutil_HuisnummerFacade2Test extends PHPUnit_Framework_TestCase
 
     public function testSplitHuisnummerMetCijferBisnummerGescheidenDoorUnderscore( )
     {
-        $this->markTestSkipped( 'This test currently fails' );
+        //$this->markTestSkipped( 'This test currently fails' );
         $label = '111_1';
         $huisnummers = $this->facade->split( $label );
         $this->assertInternalType('array', $huisnummers);
         $this->assertEquals(count($huisnummers) , 1);
-        $this->assertEquals($huisnummers[0], '111_1');
+        $this->assertEquals($huisnummers[0], '111/1');
     }
 
     public function testSplitNummerMetBusnummer( )
@@ -167,217 +167,201 @@ class KVDutil_HuisnummerFacade2Test extends PHPUnit_Framework_TestCase
         $this->assertEquals($huisnummers[2], '29');
         $this->assertEquals($huisnummers[3], '31');
     }
-/*
+
     public function testHuisnummerBereikOnevenVerschil( )
     {
         $label = '25-32';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 8);
-        $this->assertIdentical ( $huisnummers[0], '25' );
-        $this->assertIdentical ( $huisnummers[1], '26' );
-        $this->assertIdentical ( $huisnummers[2], '27' );
-        $this->assertIdentical ( $huisnummers[3], '28' );
-        $this->assertIdentical ( $huisnummers[4], '29' );
-        $this->assertIdentical ( $huisnummers[5], '30' );
-        $this->assertIdentical ( $huisnummers[6], '31' );
-        $this->assertIdentical ( $huisnummers[7], '32' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 8);
+        $this->assertEquals($huisnummers[0], '25');
+        $this->assertEquals($huisnummers[1], '26');
+        $this->assertEquals($huisnummers[2], '27');
+        $this->assertEquals($huisnummers[3], '28');
+        $this->assertEquals($huisnummers[4], '29');
+        $this->assertEquals($huisnummers[5], '30');
+        $this->assertEquals($huisnummers[6], '31');
+        $this->assertEquals($huisnummers[7], '32');
     }
 
     public function testHuisnummerBereikSpeciaal( )
     {
         $label = '25,26-31';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 7);
-        $this->assertIdentical ( $huisnummers[0], '25' );
-        $this->assertIdentical ( $huisnummers[1], '26' );
-        $this->assertIdentical ( $huisnummers[2], '27' );
-        $this->assertIdentical ( $huisnummers[3], '28' );
-        $this->assertIdentical ( $huisnummers[4], '29' );
-        $this->assertIdentical ( $huisnummers[5], '30' );
-        $this->assertIdentical ( $huisnummers[6], '31' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers) , 7);
+        $this->assertEquals($huisnummers[0], '25');
+        $this->assertEquals($huisnummers[1], '26');
+        $this->assertEquals($huisnummers[2], '27');
+        $this->assertEquals($huisnummers[3], '28');
+        $this->assertEquals($huisnummers[4], '29');
+        $this->assertEquals($huisnummers[5], '30');
+        $this->assertEquals($huisnummers[6], '31');
     }
 
     public function testCombinatieHuisnummerBereiken( )
     {
         $label = '25-31,18-26';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 9);
-        $this->assertIdentical ( $huisnummers[0], '25' );
-        $this->assertIdentical ( $huisnummers[1], '27' );
-        $this->assertIdentical ( $huisnummers[2], '29' );
-        $this->assertIdentical ( $huisnummers[3], '31' );
-        $this->assertIdentical ( $huisnummers[4], '18' );
-        $this->assertIdentical ( $huisnummers[5], '20' );
-        $this->assertIdentical ( $huisnummers[6], '22' );
-        $this->assertIdentical ( $huisnummers[7], '24' );
-        $this->assertIdentical ( $huisnummers[8], '26' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count( $huisnummers ) , 9);
+        $this->assertEquals($huisnummers[0], '25');
+        $this->assertEquals($huisnummers[1], '27');
+        $this->assertEquals($huisnummers[2], '29');
+        $this->assertEquals($huisnummers[3], '31');
+        $this->assertEquals($huisnummers[4], '18');
+        $this->assertEquals($huisnummers[5], '20');
+        $this->assertEquals($huisnummers[6], '22');
+        $this->assertEquals($huisnummers[7], '24');
+        $this->assertEquals($huisnummers[8], '26');
     }
 
     public function testBusnummerBereik( )
     {
         $label = '25 bus 3-7';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 5);
-        $this->assertIdentical ( $huisnummers[0], '25 bus 3' );
-        $this->assertIdentical ( $huisnummers[1], '25 bus 4' );
-        $this->assertIdentical ( $huisnummers[2], '25 bus 5' );
-        $this->assertIdentical ( $huisnummers[3], '25 bus 6' );
-        $this->assertIdentical ( $huisnummers[4], '25 bus 7' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 5);
+        $this->assertEquals($huisnummers[0], '25 bus 3');
+        $this->assertEquals($huisnummers[1], '25 bus 4');
+        $this->assertEquals($huisnummers[2], '25 bus 5');
+        $this->assertEquals($huisnummers[3], '25 bus 6');
+        $this->assertEquals($huisnummers[4], '25 bus 7');
     }
 
     public function testAlfaBusnummerBereik( )
     {
         $label = '25 bus C-F';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 4);
-        $this->assertIdentical ( $huisnummers[0], '25 bus C' );
-        $this->assertIdentical ( $huisnummers[1], '25 bus D' );
-        $this->assertIdentical ( $huisnummers[2], '25 bus E' );
-        $this->assertIdentical ( $huisnummers[3], '25 bus F' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 4);
+        $this->assertEquals($huisnummers[0], '25 bus C');
+        $this->assertEquals($huisnummers[1], '25 bus D');
+        $this->assertEquals($huisnummers[2], '25 bus E');
+        $this->assertEquals($huisnummers[3], '25 bus F');
     }
 
     public function testHuisnummerBereikMetLetterBisnummer( )
     {
         $label = '25C-F';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 4);
-        $this->assertIdentical ( $huisnummers[0], '25C' );
-        $this->assertIdentical ( $huisnummers[1], '25D' );
-        $this->assertIdentical ( $huisnummers[2], '25E' );
-        $this->assertIdentical ( $huisnummers[3], '25F' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 4);
+        $this->assertEquals($huisnummers[0], '25C');
+        $this->assertEquals($huisnummers[1], '25D');
+        $this->assertEquals($huisnummers[2], '25E');
+        $this->assertEquals($huisnummers[3], '25F');
     }
-    
+
     public function testHuisnummerBereikMetCijferBisnummer( )
     {
         $label = '25/3-7';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 5);
-        $this->assertIdentical ( $huisnummers[0], '25/3' );
-        $this->assertIdentical ( $huisnummers[1], '25/4' );
-        $this->assertIdentical ( $huisnummers[2], '25/5' );
-        $this->assertIdentical ( $huisnummers[3], '25/6' );
-        $this->assertIdentical ( $huisnummers[4], '25/7' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 5);
+        $this->assertEquals($huisnummers[0], '25/3');
+        $this->assertEquals($huisnummers[1], '25/4');
+        $this->assertEquals($huisnummers[2], '25/5');
+        $this->assertEquals($huisnummers[3], '25/6');
+        $this->assertEquals($huisnummers[4], '25/7');
     }
 
     public function testCombinatieBereiken( )
     {
         $label = '25C-F,28-32,29 bus 2-5';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 11);
-        $this->assertIdentical ( $huisnummers[0], '25C' );
-        $this->assertIdentical ( $huisnummers[1], '25D' );
-        $this->assertIdentical ( $huisnummers[2], '25E' );
-        $this->assertIdentical ( $huisnummers[3], '25F' );
-        $this->assertIdentical ( $huisnummers[4], '28' );
-        $this->assertIdentical ( $huisnummers[5], '30' );
-        $this->assertIdentical ( $huisnummers[6], '32' );
-        $this->assertIdentical ( $huisnummers[7], '29 bus 2' );
-        $this->assertIdentical ( $huisnummers[8], '29 bus 3' );
-        $this->assertIdentical ( $huisnummers[9], '29 bus 4' );
-        $this->assertIdentical ( $huisnummers[10], '29 bus 5' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 11);
+        $this->assertEquals($huisnummers[0], '25C');
+        $this->assertEquals($huisnummers[1], '25D');
+        $this->assertEquals($huisnummers[2], '25E');
+        $this->assertEquals($huisnummers[3], '25F');
+        $this->assertEquals($huisnummers[4], '28');
+        $this->assertEquals($huisnummers[5], '30');
+        $this->assertEquals($huisnummers[6], '32');
+        $this->assertEquals($huisnummers[7], '29 bus 2');
+        $this->assertEquals($huisnummers[8], '29 bus 3');
+        $this->assertEquals($huisnummers[9], '29 bus 4');
+        $this->assertEquals($huisnummers[10], '29 bus 5');
     }
 
     public function testBisnummerEnHuisnummerBereik( )
     {
         $label = '2A,7-11';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual ( count( $huisnummers ) , 4 );
-        $this->assertIdentical ( $huisnummers[0], '2A' );
-        $this->assertIdentical ( $huisnummers[1], '7' );
-        $this->assertIdentical ( $huisnummers[2], '9' );
-        $this->assertIdentical ( $huisnummers[3], '11' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 4);
+        $this->assertEquals($huisnummers[0], '2A');
+        $this->assertEquals($huisnummers[1], '7');
+        $this->assertEquals($huisnummers[2], '9');
+        $this->assertEquals($huisnummers[3], '11');
     }
 
     public function testBogusInput( )
     {
         $label = 'A,1/3,?';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual( count( $huisnummers ) , 3 );
-        $this->assertIdentical ( $huisnummers[0], 'A' );
-        $this->assertIdentical ( $huisnummers[1], '1/3' );
-        $this->assertIdentical ( $huisnummers[2], '?' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers), 3);
+        $this->assertEquals($huisnummers[0], 'A');
+        $this->assertEquals($huisnummers[1], '1/3');
+        $this->assertEquals($huisnummers[2], '?');
     }
 
     public function testInputWithSpaces( )
     {
         $label = ' A , 1/3 , 5 - 7 ';
         $huisnummers = $this->facade->split( $label );
-        $this->assertIsA( $huisnummers, 'array' );
-        $this->assertEqual( count( $huisnummers ) , 4 );
-        $this->assertIdentical ( $huisnummers[0], 'A' );
-        $this->assertIdentical ( $huisnummers[1], '1/3' );
-        $this->assertIdentical ( $huisnummers[2], '5' );
-        $this->assertIdentical ( $huisnummers[3], '7' );
+        $this->assertInternalType('array', $huisnummers);
+        $this->assertEquals(count($huisnummers) , 4);
+        $this->assertEquals((string) $huisnummers[0], ' A ');
+        $this->assertEquals($huisnummers[1], '1/3');
+        $this->assertEquals($huisnummers[2], '5');
+        $this->assertEquals($huisnummers[3], '7');
     }
 
     public function testMergeEenHuisnummer( )
     {
-        $array = array( '25' );
-        $label = $this->facade->merge( $array );
-        $this->assertIsA( $label, 'string' );
-        $this->assertEqual( $label, '25' );
+        $label = $this->facade->merge( '25' );
+        $this->assertEquals($this->facade->nummersToString($label), '25');
     }
 
     public function testMergeHuisnummerMetBisnummerLetter( )
     {
-        $array = array ( '25A' );
-        $label = $this->facade->merge( $array );
-        $this->assertIsA( $label, 'string' );
-        $this->assertEqual( $label, '25A' );
+        $label = $this->facade->merge( '25A' );
+        $this->assertEquals($this->facade->nummersToString($label), '25A' );
     }
 
     public function testMergeNummerMetCijferBisnummer( )
     {
-        $array = array( '25/1' );
-        $label = $this->facade->merge( $array );
-        $this->assertIsA( $label, 'string' );
-        $this->assertEqual( $label, '25/1' );
+        $labels = $this->facade->merge('25/1');
+        $this->assertEquals($this->facade->nummersToString($labels), '25/1' );
     }
 
     public function testMergeHuisnummerMetCijferBisnummerGescheidenDoorUnderscore( )
     {
-        $array = array( '111_1' );
-        $label = $this->facade->merge( $array );
-        $this->assertIsA( $label, 'string' );
-        $this->assertEqual( $label, '111_1' );
+        $labels = $this->facade->merge('111_1');
+        $this->assertEquals($this->facade->nummersToString($labels), '111/1' );
     }
-
 
     public function testMergeNummerMetBusnummer( )
     {
-        $array = array( '25 bus 3' );
-        $label = $this->facade->merge( $array );
-        $this->assertIsA( $label, 'string' );
-        $this->assertEqual( $label, '25 bus 3' );
+        $labels = $this->facade->merge( '25 bus 3' );
+        $this->assertEquals($this->facade->nummersToString($labels), '25 bus 3' );
     }
-
 
     public function testMergeHuisnummerReeks( )
     {
-        $array = '25-31';
-        $label = $this->facade->merge( $array );
-        $this->assertIsAernal( $label, 'string' );
-        $this->assertEqual( $label, '25-31' );
+        $labels = $this->facade->merge('25-31');
+        $this->assertEquals($this->facade->nummersToString($labels), '25-31');
     }
 
     public function testMergeHuisnummerBereikEvenVerschil( )
     {
-        $array = '25, 27, 29, 31';
-        $label = $this->facade->merge( $array );
-        $this->assertIsA( $label, 'string' );
-        $this->assertEqual( $label, '25-31' );
+        $labels = $this->facade->merge('25, 27, 29, 31');
+        $this->assertEquals($this->facade->nummersToString($labels), '25-31');
     }
-*/
+
 }
 
 ?>
